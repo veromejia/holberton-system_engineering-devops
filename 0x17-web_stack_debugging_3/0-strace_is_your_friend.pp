7a1a2694c3a434537665e543b1c2b4c334953fdc
veromejia <veromejia.q@gmail.com>
2020-02-19 17:51:14 -0800

    strace

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+#change extension file
+exec { 'renamefile':
+  command => 'mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
+  path    => '/bin/',
+}
