eca8f7e9bd98ffcfd8827004ba6dd6b7525ac66d
veromejia <veromejia.q@gmail.com>
2020-02-27 16:10:27 -0800

    change openfile limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 #increase the open file limit for to 4096
 exec { 'change ULIMIT':
   command => "sed -i 's/15/2000/g' /etc/default/nginx ; service nginx restart",
-  path    => ['/usr/local/bin/:/bin/']
+  path    => ['/bin', '/usr/sbin', '/usr/bin']
 }
